382dd115f9930ca8b68d064ad48767da3e58abad
valdinataalfauzan-arch <valdinataalfauzan@gmail.com>
2026-05-07 14:49:00 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,8 @@
 <?php
+// Mengizinkan semua origin (untuk development)
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 // PHP akan mencoba mengambil data asli dari Railway dulu, 
 // kalau tidak ada baru pakai data di dalam kutip.
 $host = getenv('MYSQLHOST') ?: "mysql.railway.internal";
@@ -14,3 +18,4 @@ if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
+    

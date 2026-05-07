8f206ba15c22c3b4316571aea90415c824276e6f
valdinataalfauzan-arch <valdinataalfauzan@gmail.com>
2026-05-07 14:53:03 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 // kalau tidak ada baru pakai data di dalam kutip.
 $host = getenv('MYSQLHOST') ?: "mysql.railway.internal";
 $user = getenv('MYSQLUSER') ?: "root";
-$pass = getenv('MYSQLPASSWORD') ?: "LZstNChgUtUIzpLIpzjVtJVusCMZscPX";
+$pass = getenv('MYSQLPASSWORD') ?: "xjhUtVrmswCJfWGfFtFKgLqOBnWkbIbl";
 $db   = getenv('MYSQLDATABASE') ?: "railway";
 $port = getenv('MYSQLPORT') ?: 3306;
 
